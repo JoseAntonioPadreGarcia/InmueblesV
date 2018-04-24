4f7543cd20407455220311bef888c36c9c855631
Jose Antonio <stockerssld@gmail.com>
2018-04-24 14:13:38 -0500

    Inmuebles inicial

---
+++ app/graphql/inmuebles_a_pi_schema.rb
@@ -0,0 +1,4 @@
+InmueblesAPiSchema = GraphQL::Schema.define do
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
---
+++ db/migrate/20180424180139_add_data_to_user.rb
@@ -0,0 +1,8 @@
+class AddDataToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :telefono, :string
+    add_column :users, :birthday, :date
+  end
+end
